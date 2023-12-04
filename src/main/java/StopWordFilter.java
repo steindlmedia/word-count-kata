29c114d4950875d3812392df72324c1dabc00cc7
Alexander Steindl <alexander.steindl@me.com>
2023-12-04 20:59:07 +0100

    implemented stop word filter

---
+++ src/main/java/StopWordFilter.java
@@ -0,0 +1,32 @@
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StopWordFilter {
+  List<String> stopWords;
+
+  private StopWordFilter(List<String> stopWords) {
+    this.stopWords = stopWords;
+  }
+
+  public static StopWordFilter of(List<String> stopWords) {
+    return new StopWordFilter(stopWords);
+  }
+
+  public static StopWordFilter of(InputStream inputStream) {
+    List<String> stopWords = new LinkedList<>();
+
+    try (Scanner scanner = new Scanner(inputStream)) {
+      while (scanner.hasNextLine()) {
+        stopWords.add(scanner.nextLine().trim());
+      }
+    }
+
+    return StopWordFilter.of(stopWords);
+  }
+
+  public boolean isStopWord(String word) {
+    return word != null && this.stopWords.contains(word);
+  }
+}
---
+++ src/test/java/StopWordFilterTest.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class StopWordFilterTest {
+  @Test
+  void testOf_constructStopWordList() throws IOException {
+    try (InputStream inputStream = getClass().getResourceAsStream("stopwords.txt")) {
+      StopWordFilter filter = StopWordFilter.of(inputStream);
+      Assertions.assertEquals(List.of("the", "a", "on", "off"), filter.stopWords);
+    }
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"the", "a", "on", "off"})
+  void testIsStopWord_stopWord_returnsTrue(String stopWord) {
+    StopWordFilter filter = StopWordFilter.of(List.of("the", "a", "on", "off"));
+    Assertions.assertTrue(filter.isStopWord(stopWord));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"this", "an", "I"})
+  void testIsStopWord_nonStopWord_returnsFalse(String nonStopWord) {
+    StopWordFilter filter = StopWordFilter.of(List.of("the", "a", "on", "off"));
+    Assertions.assertFalse(filter.isStopWord(nonStopWord));
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void testIsStopWord_nullOrEmpty_returnsFalse(String nonStopWord) {
+    StopWordFilter filter = StopWordFilter.of(List.of("the", "a", "on", "off"));
+    Assertions.assertFalse(filter.isStopWord(nonStopWord));
+  }
+}
