111b6b0201c53cfcc4954558d47b6dc61d3b7a98
Alexander Steindl <alexander.steindl@me.com>
2023-12-04 20:32:06 +0100

    implemented java application

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,22 @@
+import java.io.IOException;
+
 public class JavaApplication {
+  private final IncomingPort adapter;
+  private final OutgoingPort outgoingAdapter;
+
+  public JavaApplication(IncomingPort adapter, OutgoingPort outgoingAdapter) {
+    this.adapter = adapter;
+    this.outgoingAdapter = outgoingAdapter;
+  }
+
+  public void run() throws IOException {
+    String input = adapter.readInput();
+    WordCountService wordCountService = new WordCountService(new Tokenizer());
+    outgoingAdapter.writeOutput(wordCountService.countWords(input));
+  }
+
+  public static void main(String[] args) throws IOException {
+    JavaApplication application = new JavaApplication(new IncomingAdapter(System.in, System.out), new OutgoingAdapter(System.out));
+    application.run();
+  }
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,35 @@
-public class JavaApplicationTest {
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class JavaApplicationTest {
+  @Test
+  void testRun() throws IOException {
+    String input = "Mary had a little lamb";
+
+    InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    JavaApplication application = new JavaApplication(new IncomingAdapter(inputStream, outputStream), new OutgoingAdapter(outputStream));
+    application.run();
+
+    Assertions.assertEquals("Enter text: Number of words: 5\n", outputStream.toString());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"", " "})
+  void testRun_emptyString(String input) throws IOException {
+    InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    JavaApplication application = new JavaApplication(new IncomingAdapter(inputStream, outputStream), new OutgoingAdapter(outputStream));
+    application.run();
+
+    Assertions.assertEquals("Enter text: Number of words: 0\n", outputStream.toString());
+  }
 }
